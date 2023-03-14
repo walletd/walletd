a1ea4f6d6dc96ae4c8e8df81d954a81d68548d58
Devin <devin@blackhat.co.za>
2023-03-14 13:35:30 +0200

    refactor: move coins to submodules (#88)
    * refactor: removed old coins files
    
    * refactor: added coins as submodules

--- coins/README.md
+++
@@ -1,6 +0,0 @@
-README for WalletD coins crate
-
-Create an instance of a BTC CryptoCoin
-```rust
-let crypto_coin = CryptoCoin::from_str("BTC");
-```
--- coins/monero/examples/bulletproof_plus.rs
+++
@@ -1,9 +0,0 @@
-extern crate walletd_monero;
-
-use monero_generators::bulletproofs_generators;
-
-fn main() {
-    println!("Bulletproof+ example");
-    let _generators = bulletproofs_generators(b"bulletproof_plus");
-    println!("done generating example");
-}
