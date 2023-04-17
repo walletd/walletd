8a12ee36d2952a3f609348920583e6af75e86eb7
amilumibe <111754655+amilumibe@users.noreply.github.com>
2023-04-17 13:19:59 -0400

    refactor: change hd_key_basic example to import from seed hex

--- mnemonics/hd_key/examples/hd_key_basic.rs
+++ mnemonics/hd_key/examples/hd_key_basic.rs
@@ -1,17 +1,17 @@
 use walletd_hd_key::{HDKey, HDNetworkType, Seed};
+use std::str::FromStr;
 
 fn main() -> () {
+
+    let seed_hex = "a2fd9c0522d84d52ee4c8533dc02d4b69b4df9b6255e1af20c9f1d4d691689f2a38637eb1ec778972bf845c32d5ae83c7536999b5666397ac32021b21e0accee";
+    println!("seed_hex: {}", seed_hex);
+
     let keys = HDKey::new_master(
-        Seed::new(vec![
-            162, 253, 156, 5, 34, 216, 77, 82, 238, 76, 133, 51, 220, 2, 212, 182, 155, 77, 249,
-            182, 37, 94, 26, 242, 12, 159, 29, 77, 105, 22, 137, 242, 163, 134, 55, 235, 30, 199,
-            120, 151, 43, 248, 69, 195, 45, 90, 232, 60, 117, 54, 153, 155, 86, 102, 57, 122, 195,
-            32, 33, 178, 30, 10, 204, 238,
-        ]),
+        Seed::from_str(&seed_hex).unwrap(),
         HDNetworkType::MainNet,
     )
     .unwrap();
-    println!("{:?}", keys);
+    println!("{:#?}", keys);
 
-    println!("{}", keys.to_wif().unwrap());
+    println!("wif of master hd key {}", keys.to_wif().unwrap());
 }
