36768bec1c2189f59c50607d1d6150fe8b52bd67
amilumibe <111754655+amilumibe@users.noreply.github.com>
2023-04-18 15:08:28 -0400

    refactor: move eth integration test out of lib.rs

--- coins/ethereum/src/lib.rs
+++ coins/ethereum/src/lib.rs
@@ -35,33 +35,3 @@ impl fmt::Display for EthereumFormat {
         }
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::str::FromStr;
-    use walletd_bip39::Seed;
-    use walletd_coin_model::{CryptoWallet, CryptoWalletBuilder};
-    use walletd_hd_key::HDNetworkType;
-
-    #[test]
-    fn test_wallet_instantiation_from_mnemonic_seed() {
-        let seed_hex = "a2fd9c0522d84d52ee4c8533dc02d4b69b4df9b6255e1af20c9f1d4d691689f2a38637eb1ec778972bf845c32d5ae83c7536999b5666397ac32021b21e0accee";
-        let seed = Seed::from_str(seed_hex).unwrap();
-        let wallet = EthereumWallet::builder()
-            .with_mnemonic_seed(seed)
-            .with_network_type(HDNetworkType::TestNet)
-            .build()
-            .unwrap();
-
-        assert_eq!(
-            &wallet.public_address(),
-            "0x6EEb11eA2905fEe101f72BF94F792dbc2dfB42B7"
-        );
-        assert_eq!(
-            format!("{:#x}", &wallet.private_key().unwrap()),
-            "0xa5dcdaefa08013092ca37d3f60d46f27510df8777a3a7dd6a1b9f373352caa75"
-        );
-        assert_eq!(wallet.network(), HDNetworkType::TestNet);
-    }
-}
