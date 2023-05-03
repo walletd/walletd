4370d706f422d33ce151fe6a79b8673dc4c939be
amilumibe <111754655+amilumibe@users.noreply.github.com>
2023-05-02 22:18:53 -0400

    feat: add Bip39MnemonicType to walletd_bip39 prelude

--- mnemonics/bip39/src/prelude.rs
+++ mnemonics/bip39/src/prelude.rs
@@ -5,6 +5,6 @@
 //! use walletd_bip39::prelude::*;
 //! ```
 
-pub use crate::{Bip39Mnemonic, Bip39MnemonicBuilder, Bip39Language};
+pub use crate::{Bip39Mnemonic, Bip39MnemonicBuilder, Bip39Language, Bip39MnemonicType};
 pub use walletd_mnemonics_core::prelude::*;
 
