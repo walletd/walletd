de0bff30af2dc804380afa26abb25e53e3c0ca4b
amilumibe <111754655+amilumibe@users.noreply.github.com>
2023-07-03 18:02:05 -0400

    feat: add lock state for keystore

--- walletd/src/keystore.rs
+++ walletd/src/keystore.rs
@@ -1,2 +1,15 @@
+
+
 /// Stores the relevant keys and controls access to them
-pub struct KeyStore;
+pub struct KeyStore<S: LockState> {
+    /// Whether the keystore is locked or unlocked
+    pub lock_state: S,
+}
+
+// State type options
+struct Locked;
+struct Unlocked;
+
+pub trait LockState {}
+impl LockState for Locked {}
+impl LockState for Unlocked {}
