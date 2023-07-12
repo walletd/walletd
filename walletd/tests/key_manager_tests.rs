b3092ebb13437955e3ef43b8a774eac75e367df5
amilumibe <111754655+amilumibe@users.noreply.github.com>
2023-07-12 15:45:23 -0400

    test (wip): addition println, currently this dir org does not file encrypt/decrypt test

--- walletd/tests/key_manager_tests.rs
+++ walletd/tests/key_manager_tests.rs
@@ -4,6 +4,7 @@ use std::path::Path;
 
 #[test]
 fn test_streaming_symmetric_encrypt_decrypt_file() {
+    println!("Running test streaming symmetric encrypt/decrypt file");
     let mut data_file = File::create(Path::new("../test_encrypt_file.txt")).unwrap();
     data_file.write("Hello World!".as_bytes()).unwrap();
     // read file
