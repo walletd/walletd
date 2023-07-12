76a7cfd861e97b293e63cc17ac182c1780c7f1c1
amilumibe <111754655+amilumibe@users.noreply.github.com>
2023-07-12 17:55:08 -0400

    refactor (wip): test to write file is running path location not ideal

--- walletd/tests/key_manager_tests.rs
+++ walletd/tests/key_manager_tests.rs
@@ -1,16 +1,16 @@
-#[cfg(test)]
-mod tests {
-    use std::fs::File;
-    use std::io::{Read, Write};
-    use std::path::Path;
+use std::fs::File;
+use std::io::{Read, Write};
+use std::path::Path;
     
-    #[test]
-    fn test_streaming_symmetric_encrypt_decrypt_file() {
-        println!("Running test streaming symmetric encrypt/decrypt file");
-        let mut data_file = File::create(Path::new("../test_encrypt_file.txt")).unwrap();
-        data_file.write("Hello World!".as_bytes()).unwrap();
-        // read file
+#[test]
+fn test_streaming_symmetric_encrypt_decrypt_file() {
+    println!("Running test streaming symmetric encrypt/decrypt file");
+    let mut data_file = File::create(Path::new("./test_encrypt_file.txt")).unwrap();
+    data_file.write("Hello World!".as_bytes()).unwrap();
+    // read file
 
-        // write out the contents of the file
-    }
+    // write out the contents of the file
+
+
 }
+
