d79dbb1c7860387a1d9e4ed0dbdbd7cdd1b723fb
amilumibe <111754655+amilumibe@users.noreply.github.com>
2023-07-12 18:40:52 -0400

    test: able to create file and read its contents

--- walletd/tests/key_manager_tests.rs
+++ walletd/tests/key_manager_tests.rs
@@ -1,16 +1,16 @@
 use std::fs::File;
 use std::io::{Read, Write};
 use std::path::Path;
+use std::fs;
+
     
 #[test]
 fn test_streaming_symmetric_encrypt_decrypt_file() {
     println!("Running test streaming symmetric encrypt/decrypt file");
-    let mut data_file = File::create(Path::new("./test_encrypt_file.txt")).unwrap();
-    data_file.write("Hello World!".as_bytes()).unwrap();
-    // read file
-
-    // write out the contents of the file
-
-
+    let file_path = "./tests/generated_or_modified_files/test_encrypt_file.txt";
+    let mut data_file = File::create(Path::new(file_path)).unwrap();
+    assert!(data_file.write("Hello World!".as_bytes()).is_ok());
+    let contents = fs::read_to_string(file_path).unwrap();
+    println!("File has contents:\n{contents}");
 }
 
