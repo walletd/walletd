5576dc44312438e628eb6cb68f7737579b1ca001
amilumibe <111754655+amilumibe@users.noreply.github.com>
2023-07-13 14:25:15 -0400

    test (wip): encryption is working decryption not yet

--- walletd/tests/key_manager_tests.rs
+++ walletd/tests/key_manager_tests.rs
@@ -1,12 +1,11 @@
 use std::fs::File;
 use std::io::Write;
 use std::path::Path;
+use std::io::prelude::*;
 use std::fs;
 use crypto::aessafe::{AesSafe128Encryptor, AesSafe128Decryptor};
 use rand::{thread_rng, Rng};
 use aesstream::{AesWriter, AesReader};
-
-
 
 #[test]
 fn test_unlocked_file_create_read() {
@@ -24,20 +23,21 @@ fn test_streaming_symmetric_encrypt_decrypt_file() {
     // wrap a file with encryption
     let key: [u8; 16] = thread_rng().gen();
     let file_path = "./tests/generated_or_modified_files/test_encrypt_file.txt";
-    
+    let unencrypted_message = "Hello World, is this encrypted?";
     let mut data_file = File::create(Path::new(file_path)).unwrap();
     let encryptor = AesSafe128Encryptor::new(&key);
     let mut writer = AesWriter::new(data_file, encryptor).unwrap();
-    assert!(writer.write_all("Hello World, is this encrypted?".as_bytes()).is_ok());
+    assert!(writer.write_all(unencrypted_message.as_bytes()).is_ok());
+    println!("unencrypted message: {}", unencrypted_message);
 
     let key: [u8; 16] = thread_rng().gen();
-    let read_file = File::open(Path::new(file_path)).unwrap();
-    let decryptor = AesSafe128Decryptor::new(&key);
-    let mut reader = AesReader::new(read_file, decryptor).unwrap();
-    let mut decrypted = String::new();
-    // assert!(reader.read_to_string(&mut decrypted).is_ok());
-    // assert_eq!(decrypted, "Hello World, is this encrypted?");
+    let mut read_file = File::open(Path::new(file_path)).unwrap();
+    let mut buffer = Vec::new();
+    read_file.read_to_end(&mut buffer).unwrap();
 
-    
+    //let decryptor = AesSafe128Decryptor::new(&key);
+    //let mut reader = AesReader::new(read_file, decryptor).unwrap();
+    //let mut decrypted = Vec::new();
+
 }
 
