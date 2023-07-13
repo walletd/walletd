c2ab9cfa72bc302ce6b65c350703c7c6786110cb
amilumibe <111754655+amilumibe@users.noreply.github.com>
2023-07-13 13:50:31 -0400

    refactor: take out from gitignore the tests subfolder for files

--- walletd/tests/key_manager_tests.rs
+++ walletd/tests/key_manager_tests.rs
@@ -3,16 +3,15 @@ use std::io::Write;
 use std::path::Path;
 use std::fs;
 use crypto::aessafe::{AesSafe128Encryptor, AesSafe128Decryptor};
-use rand::Rng;
-use rand::rngs::OsRng;
-use aesstream;
+use rand::{thread_rng, Rng};
+use aesstream::{AesWriter, AesReader};
 
 
 
 #[test]
 fn test_unlocked_file_create_read() {
     println!("Running test unlocked file create read");
-    let file_path = "./tests/generated_or_modified_files/test_encrypt_file.txt";
+    let file_path = "./tests/generated_or_modified_files/test_create_file.txt";
     let mut data_file = File::create(Path::new(file_path)).unwrap();
     assert!(data_file.write("Hello World!".as_bytes()).is_ok());
     let contents = fs::read_to_string(file_path).unwrap();
@@ -23,5 +22,22 @@ fn test_unlocked_file_create_read() {
 fn test_streaming_symmetric_encrypt_decrypt_file() {
     println!("Streaming symmetric encrypt/decrypt file");
     // wrap a file with encryption
+    let key: [u8; 16] = thread_rng().gen();
+    let file_path = "./tests/generated_or_modified_files/test_encrypt_file.txt";
+    
+    let mut data_file = File::create(Path::new(file_path)).unwrap();
+    let encryptor = AesSafe128Encryptor::new(&key);
+    let mut writer = AesWriter::new(data_file, encryptor).unwrap();
+    assert!(writer.write_all("Hello World, is this encrypted?".as_bytes()).is_ok());
+
+    let key: [u8; 16] = thread_rng().gen();
+    let read_file = File::open(Path::new(file_path)).unwrap();
+    let decryptor = AesSafe128Decryptor::new(&key);
+    let mut reader = AesReader::new(read_file, decryptor).unwrap();
+    let mut decrypted = String::new();
+    // assert!(reader.read_to_string(&mut decrypted).is_ok());
+    // assert_eq!(decrypted, "Hello World, is this encrypted?");
+
+    
 }
 
