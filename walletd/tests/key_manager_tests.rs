52171023b999efd0e3065c1d3ce843ae9e98150e
amilumibe <111754655+amilumibe@users.noreply.github.com>
2023-07-13 12:22:56 -0400

    refactor (test): mv of file create/read to separate test

--- walletd/tests/key_manager_tests.rs
+++ walletd/tests/key_manager_tests.rs
@@ -1,16 +1,27 @@
 use std::fs::File;
-use std::io::{Read, Write};
+use std::io::Write;
 use std::path::Path;
 use std::fs;
+use crypto::aessafe::{AesSafe128Encryptor, AesSafe128Decryptor};
+use rand::Rng;
+use rand::rngs::OsRng;
+use aesstream;
 
-    
+
+
 #[test]
-fn test_streaming_symmetric_encrypt_decrypt_file() {
-    println!("Running test streaming symmetric encrypt/decrypt file");
+fn test_unlocked_file_create_read() {
+    println!("Running test unlocked file create read");
     let file_path = "./tests/generated_or_modified_files/test_encrypt_file.txt";
     let mut data_file = File::create(Path::new(file_path)).unwrap();
     assert!(data_file.write("Hello World!".as_bytes()).is_ok());
     let contents = fs::read_to_string(file_path).unwrap();
     println!("File has contents:\n{contents}");
+}
+
+#[test]
+fn test_streaming_symmetric_encrypt_decrypt_file() {
+    println!("Streaming symmetric encrypt/decrypt file");
+    // wrap a file with encryption
 }
 
