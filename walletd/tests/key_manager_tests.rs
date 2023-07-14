aba7de333112c9603c1e203829e84a8c5c642043
amilumibe <111754655+amilumibe@users.noreply.github.com>
2023-07-14 11:49:00 -0400

    test (wip): checking encrypt part

--- walletd/tests/key_manager_tests.rs
+++ walletd/tests/key_manager_tests.rs
@@ -21,9 +21,28 @@ fn key_manager_test_unlocked_file_create_read() {
 }
 
 #[test]
-fn key_manager_test_encrypt_decrypt_file_with_passphrase() {
+fn key_manager_test_encrypt_decrypt_string_with_passphrase() {
+    println!("Running test encrypt/decrypt string with passphrase");
+    let passphrase = "my_master_passphrase";
+    println!("passphrase: {}", passphrase);
+    let key = GenericArray::from_slice(passphrase.as_bytes());
+    println!("passphrase as key: {:?}", key);
+    let cipher = ChaCha20Poly1305::new(&key);
+    let mut rand_for_nonce: [u8; 12] = thread_rng().gen();
+    println!("rand_for_nonce: {:?}", rand_for_nonce);
+    // sorting the nonce so it's easier to compare if same or different (need to use the same nonce to decrypt the message, should avoid reuse of same nonce for different messages)
+    rand_for_nonce.sort();
+    let nonce = GenericArray::from_slice(rand_for_nonce.as_ref());
+    println!("sorted nonce: {:?}", nonce);
+    let plaintext = "hexadecimal";
+    println!("plaintext message: {}", plaintext);
+    let plaintext_bytes = plaintext.as_bytes();
+    println!("plaintext message as bytes: {:?}", plaintext_bytes);
+    let ciphertext_bytes = cipher.encrypt(&nonce, plaintext_bytes).unwrap();
+    println!("encrypted message (bytes): {:?}", ciphertext_bytes);
+    
+    // next is the decryption
     todo!()
-
 }
 
 #[test]
