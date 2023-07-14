62a98849eb39458b2339d8260ad387858879e8a1
amilumibe <111754655+amilumibe@users.noreply.github.com>
2023-07-14 13:22:23 -0400

    test (wip but runs): generic array test

--- walletd/tests/key_manager_tests.rs
+++ walletd/tests/key_manager_tests.rs
@@ -21,27 +21,35 @@ fn key_manager_test_unlocked_file_create_read() {
 }
 
 #[test]
+fn key_manager_check_generic_array() {
+    println!("Running test key_manager check generic array");
+    let bytes: [u8; 4] = [1, 2, 3, 4];
+    println!("bytes: {:?}", bytes);
+    let generic_array = GenericArray::from(bytes);
+    println!("generic_array: {:?}", generic_array);
+}
+
+#[test]
 fn key_manager_test_encrypt_decrypt_string_with_passphrase() {
     println!("Running test encrypt/decrypt string with passphrase");
     let passphrase = "my_master_passphrase";
-    println!("passphrase: {}", passphrase);
-    /* let key = GenericArray::from_slice(passphrase.as_bytes());
+    let passphrase_bytes = passphrase.as_bytes();
+    println!("passphrase: {} as bytes {:?}", passphrase, passphrase_bytes);
+    let key = GenericArray::from_slice(passphrase.as_bytes());
     println!("passphrase as key: {:?}", key);
     let cipher = ChaCha20Poly1305::new(&key);
     let mut rand_for_nonce: [u8; 12] = thread_rng().gen();
     println!("rand_for_nonce: {:?}", rand_for_nonce);
     // sorting the nonce so it's easier to compare if same or different (need to use the same nonce to decrypt the message, should avoid reuse of same nonce for different messages)
     rand_for_nonce.sort();
-    let nonce = GenericArray::from_slice(rand_for_nonce.as_ref());
-    println!("sorted nonce: {:?}", nonce);
-    let plaintext = "hexadecimal";
-    println!("plaintext message: {}", plaintext);
-    let plaintext_bytes = plaintext.as_bytes();
-    println!("plaintext message as bytes: {:?}", plaintext_bytes);
-    let ciphertext_bytes = cipher.encrypt(&nonce, plaintext_bytes).unwrap();
-    println!("encrypted message (bytes): {:?}", ciphertext_bytes);
-    */
-    
+    // let nonce = GenericArray::from_slice(rand_for_nonce.as_ref());
+    // println!("sorted nonce: {:?}", nonce);
+    // let plaintext = "hexadecimal";
+    // println!("plaintext message: {}", plaintext);
+    // let plaintext_bytes = plaintext.as_bytes();
+    // println!("plaintext message as bytes: {:?}", plaintext_bytes);
+    // let ciphertext_bytes = cipher.encrypt(&nonce, plaintext_bytes).unwrap();
+    // println!("encrypted message (bytes): {:?}", ciphertext_bytes);
     // next is the decryption
 }
 
