00717846f45495cef56c0d23ee2e02133bb9520f
amilumibe <111754655+amilumibe@users.noreply.github.com>
2023-07-14 11:06:20 -0400

    refactor (test: wip): change names of tests to start with key_manager

--- walletd/tests/key_manager_tests.rs
+++ walletd/tests/key_manager_tests.rs
@@ -11,7 +11,7 @@ use chacha20poly1305::aead::Aead;
 use generic_array::GenericArray;
 
 #[test]
-fn test_unlocked_file_create_read() {
+fn key_manager_test_unlocked_file_create_read() {
     println!("Running test unlocked file create read");
     let file_path = "./tests/generated_or_modified_files/test_create_file.txt";
     let mut data_file = File::create(Path::new(file_path)).unwrap();
@@ -21,36 +21,17 @@ fn test_unlocked_file_create_read() {
 }
 
 #[test]
-fn test_streaming_symmetric_encrypt_decrypt_file() {
-    println!("Streaming symmetric encrypt/decrypt file");
-    // wrap a file with encryption
-    let key: [u8; 16] = thread_rng().gen();
-    let file_path = "./tests/generated_or_modified_files/test_encrypt_file.txt";
-    let unencrypted_message = "Hello World, is this encrypted?";
-    let mut data_file = File::create(Path::new(file_path)).unwrap();
-    // assert!(writer.write_all(unencrypted_message.as_bytes()).is_ok());
-    println!("unencrypted message: {}", unencrypted_message);
+fn key_manager_test_encrypt_decrypt_file_with_passphrase() {
+    todo!()
 
-    let key: [u8; 16] = thread_rng().gen();
-    let mut read_file = File::open(Path::new(file_path)).unwrap();
-    let mut buffer = Vec::new();
-    read_file.read_to_end(&mut buffer).unwrap();
 }
 
 #[test]
-fn test_encrypt_decrypt_with_passphrase() {
-    println!("Test encrypt/decrypt with passphrase");
-    let passphrase = "my_master_passphrase";
-    let key = GenericArray::from_slice(passphrase.as_bytes());
-    let cipher = ChaCha20Poly1305::new(&key);
-    let rand_for_nonce: [u8; 12] = thread_rng().gen();
-    // sorting the nonce so it's easier to compare if same or different (need to use the same nonce to decrypt the message, should avoid reuse of same nonce for different messages)
-    let nonce = GenericArray::from_slice(&rand_for_nonce);
-    let plaintext = b"hexadecimal";
-    let ciphertext_bytes = cipher.encrypt(&nonce, plaintext.as_ref()).unwrap();
-    let ciphertext = String::from_utf8(ciphertext_bytes).unwrap();
-    println!("Encrypt is done");
-    println!("Output of the encryption: {}", ciphertext);
+fn key_manager_test_streaming_symmetric_encrypt_decrypt_file() {
+    todo!()
 }
+
+
+
 
 
