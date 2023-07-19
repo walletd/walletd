c109ecbbfe4daf0d26b2a589c557d46925b3a701
Karl Buys <kbbuys@gmail.com>
2023-07-19 16:31:19 +0200

    docs: adds additional example for getting transaction and data by hash

---
+++ coins/ethereum/examples/get_transaction_data.rs
@@ -0,0 +1,46 @@
+extern crate walletd_ethereum;
+// https://goerli.etherscan.io/block/8455626
+use std::sync::Arc;
+
+use ethers::prelude::*;
+use ethers::{
+    abi::Abi,
+    types::{Address, H256},
+};
+use serde;
+use walletd_coin_core::BlockchainConnector;
+use walletd_ethereum::EthClient;
+use serde_json::json;
+use serde::{Deserialize, Serialize};
+
+pub const PROVIDER_URL: &str = "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161";
+
+#[tokio::main]
+async fn main() {
+    // Transport can be one of Http, WebSocket, Ipc
+    // let transport = web3::transports::Http::new(PROVIDER_URL)?;
+
+    let ethclient_url = "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161";
+    let eth_client = EthClient::new(ethclient_url).unwrap();
+
+    let block_data = eth_client
+            .ethers()
+            .get_block_with_txs(ethers::types::BlockId::Number(
+                ethers::types::BlockNumber::Latest,
+            ))
+            .await
+            .unwrap()
+            .unwrap();
+
+        let output_block_data = block_data.clone();
+        for i in 0..10 {
+            println!("{:?}", output_block_data.transactions[i]);
+        }
+        // Ok(output_block_data)
+        
+        let tx_hash = output_block_data.transactions[0].hash;
+        let tx_data = eth_client.get_transaction_data_from_tx_hash(tx_hash).await.unwrap();
+
+        println!("tx_data: {:?}", tx_data);
+
+}
