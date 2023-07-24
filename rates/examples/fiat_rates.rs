08ede9f02d05bf4e2917a7d51de975b6a190202b
Devin <devin@blackhat.co.za>
2023-07-24 13:01:20 +0200

    feat: set standard return type for fiat rates

--- rates/examples/fiat_rates.rs
+++ rates/examples/fiat_rates.rs
@@ -1,4 +1,14 @@
+use walletd_rates::{ExchangeRateApi, ExchangeRateHost};
+
 #[tokio::main]
 async fn main() {
-    walletd_rates::get_rate2().await;
+    let result = ExchangeRateApi::new("78b121ef7b842f85c127c1ab")
+        .get_rate()
+        .await
+        .unwrap();
+    dbg!(result);
+
+    // let result = ExchangeRateHost::new().get_rate().await.unwrap();
+    // dbg!(result);
+    //walletd_rates::get_rate2().await;
 }
--- rates/src/lib.rs
+++ rates/src/lib.rs
@@ -15,33 +15,71 @@ pub async fn get_rate() {
     println!("{:?}", body);
 }
 
+pub trait FiatRateProvider {
+    fn get_rate(&self) -> Result<FiatRates, reqwest::Error>;
+}
+
+#[derive(Debug)]
+pub enum Providers {
+    ExchangeRateApi,
+    ExchangeRateHost,
+}
+
+#[derive(Debug)]
+pub struct FiatRates {
+    provider: Providers,
+    base: String,
+    date: String,
+    rates: HashMap<String, f32>,
+}
+
 #[derive(Deserialize, Debug)]
-struct BaseResponse2 {
+pub struct BaseResponse2 {
     result: String,
-    documentation: String,
-    terms_of_use: String,
+    // documentation: String
+    // terms_of_use: String,
     time_last_update_unix: u64,
-    base_code: String,
-    conversion_rates: HashMap<String, f32>,
+    #[serde(rename = "base_code")]
+    base: String,
+    #[serde(rename = "conversion_rates")]
+    rates: HashMap<String, f32>,
 }
 
-pub async fn get_rate2() {
-    let body =
-        reqwest::get("https://v6.exchangerate-api.com/v6/78b121ef7b842f85c127c1ab/latest/USD")
-            .await
-            .unwrap()
-            .json::<BaseResponse2>()
-            .await
-            .unwrap();
-    // let block_count = body
-    //     .parse::<u64>()
-    //     .map_err(|e| Error::FromStr(e.to_string()))?;
-    println!("{:?}", body);
+pub struct ExchangeRateApi {
+    api_key: String,
+}
+
+impl ExchangeRateApi {
+    pub fn new(api_key: &str) -> Self {
+        Self {
+            api_key: api_key.to_string(),
+        }
+    }
+
+    pub async fn get_rate(&self) -> Result<FiatRates, reqwest::Error> {
+        let body = reqwest::get(format!(
+            "https://v6.exchangerate-api.com/v6/{}/latest/USD",
+            self.api_key,
+        ))
+        .await
+        .unwrap()
+        .json::<BaseResponse2>()
+        .await
+        .unwrap();
+
+        let fiat_rates = FiatRates {
+            provider: Providers::ExchangeRateApi,
+            base: body.base,
+            date: body.time_last_update_unix.to_string(),
+            rates: body.rates,
+        };
+        Ok(fiat_rates)
+    }
 }
 
 #[derive(Deserialize, Debug)]
-struct BaseResponse {
-    motd: Motd,
+pub struct BaseResponse {
+    //motd: Motd,
     success: bool,
     base: String,
     date: String,
@@ -59,16 +97,27 @@ struct Motd {
     url: String,
 }
 
-pub async fn get_rate3() {
-    let body = reqwest::get("https://api.exchangerate.host/latest?base=usd")
-        .await
-        .unwrap()
-        .json::<BaseResponse>()
-        .await
-        .unwrap();
+pub struct ExchangeRateHost {}
 
-    // let block_count = body
-    //     .parse::<u64>()
-    //     .map_err(|e| Error::FromStr(e.to_string()))?;
-    println!("{:?}", body);
+impl ExchangeRateHost {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub async fn get_rate(&self) -> Result<FiatRates, reqwest::Error> {
+        let body = reqwest::get("https://api.exchangerate.host/latest?base=usd")
+            .await
+            .unwrap()
+            .json::<BaseResponse>()
+            .await
+            .unwrap();
+
+        let fiat_rates = FiatRates {
+            provider: Providers::ExchangeRateHost,
+            base: body.base,
+            date: body.date,
+            rates: body.rates,
+        };
+        Ok(fiat_rates)
+    }
 }
