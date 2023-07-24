6aafcd7919427ff834b6658a2496fe3b55ed0c27
Karl Buys <kbbuys@gmail.com>
2023-07-24 13:19:07 +0200

    docs: create example showing smart contract calls
    Added example of how one would use walletd to query uniswap for liquidity

--- coins/ethereum/examples/uniswap_v2_pair_query.rs
+++ coins/ethereum/examples/uniswap_v2_pair_query.rs
@@ -3,6 +3,7 @@ extern crate walletd_ethereum;
 use std::sync::Arc;
 
 use ethers::prelude::*;
+<<<<<<< HEAD
 use ethers::types::Address;
 
 use walletd_coin_core::BlockchainConnector;
@@ -13,6 +14,20 @@ use ethers::{
     contract::abigen,
     providers::{Http, Provider},
 };
+=======
+use ethers::{
+    abi::Abi,
+    types::{Address, H256},
+};
+use serde;
+use walletd_coin_core::BlockchainConnector;
+use walletd_ethereum::EthClient;
+use serde_json::json;
+use serde::{Deserialize, Serialize};
+
+pub const PROVIDER_URL: &str = "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161";
+
+>>>>>>> 38750f4 (docs: create example showing smart contract calls)
 
 // Generate the type-safe contract bindings by providing the ABI
 // definition in human readable format
@@ -23,6 +38,7 @@ abigen!(
     ]"#,
 );
 
+<<<<<<< HEAD
 abigen!(
     ERC20,
     r#"[
@@ -40,6 +56,18 @@ async fn main() {
         .parse::<Address>()
         .unwrap();
     let pair = IUniswapV2Pair::new(address, Arc::clone(&client));
+=======
+#[tokio::main]
+async fn main() {
+    let ethclient_url = "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161";
+    let eth_client = EthClient::new(ethclient_url).unwrap();
+
+    let eth_client = Arc::new(eth_client);
+
+    // ETH/USDT pair on Uniswap V2
+    let address = "0x0d4a11d5EEaaC28EC3F61d100daF4d40471f1852".parse::<Address>().unwrap();
+    let pair = IUniswapV2Pair::new(address, Arc::clone(&eth_client.ethers()));
+>>>>>>> 38750f4 (docs: create example showing smart contract calls)
 
     // getReserves -> get_reserves
     let (reserve0, reserve1, _timestamp) = pair.get_reserves().call().await.unwrap();
@@ -47,6 +75,7 @@ async fn main() {
 
     let mid_price = f64::powi(10.0, 18 - 6) * reserve1 as f64 / reserve0 as f64;
     println!("ETH/USDT price: {mid_price:.2}");
+<<<<<<< HEAD
 
     let address = "0x0d4a11d5EEaaC28EC3F61d100daF4d40471f1852"
         .parse::<Address>()
@@ -56,4 +85,6 @@ async fn main() {
     let balance = instance.balance_of(address).call().await.unwrap();
 
     println!("balance: {:?}", balance);
+=======
+>>>>>>> 38750f4 (docs: create example showing smart contract calls)
 }
