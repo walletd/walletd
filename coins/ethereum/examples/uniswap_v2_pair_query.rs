504615823347edd9ec70b6ae71dedf1827a8f9e6
Karl Buys <kbbuys@gmail.com>
2023-07-25 16:35:21 +0200

    docs: clean up example that queries uniswap liquidity

--- coins/ethereum/examples/uniswap_v2_pair_query.rs
+++ coins/ethereum/examples/uniswap_v2_pair_query.rs
@@ -13,7 +13,13 @@ use walletd_ethereum::EthClient;
 use serde_json::json;
 use serde::{Deserialize, Serialize};
 
-pub const PROVIDER_URL: &str = "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161";
+pub const PROVIDER_URL: &str = "https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161";
+
+
+use ethers::{
+    contract::abigen,
+    providers::{Http, Provider}
+};
 
 
 // Generate the type-safe contract bindings by providing the ABI
@@ -25,16 +31,18 @@ abigen!(
     ]"#,
 );
 
-#[tokio::main]
-async fn main() {
-    let ethclient_url = "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161";
-    let eth_client = EthClient::new(ethclient_url).unwrap();
+abigen!(ERC20, r#"[
+        function balanceOf(address account) external view returns (uint256)
+    ]"#);
 
-    let eth_client = Arc::new(eth_client);
+#[tokio::main]
+async fn main() -> () {
+    let client = Provider::<Http>::try_from(PROVIDER_URL).unwrap();
+    let client = Arc::new(client);
 
     // ETH/USDT pair on Uniswap V2
     let address = "0x0d4a11d5EEaaC28EC3F61d100daF4d40471f1852".parse::<Address>().unwrap();
-    let pair = IUniswapV2Pair::new(address, Arc::clone(&eth_client.ethers()));
+    let pair = IUniswapV2Pair::new(address, Arc::clone(&client));
 
     // getReserves -> get_reserves
     let (reserve0, reserve1, _timestamp) = pair.get_reserves().call().await.unwrap();
@@ -42,4 +50,14 @@ async fn main() {
 
     let mid_price = f64::powi(10.0, 18 - 6) * reserve1 as f64 / reserve0 as f64;
     println!("ETH/USDT price: {mid_price:.2}");
+
+    
+    let address = "0x0d4a11d5EEaaC28EC3F61d100daF4d40471f1852".parse::<Address>().unwrap();
+    let instance = ERC20::new(address, Arc::clone(&client));
+
+    let balance = instance.balance_of(address).call().await.unwrap();
+
+    println!("balance: {:?}", balance);
+
+    ()
 }
