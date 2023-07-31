9e4cda1498f5b040d2be55b67e3eee15952510e9
Karl Buys <kbbuys@gmail.com>
2023-07-31 11:44:54 +0200

    feat: add allowance function to ethclient

--- coins/ethereum/src/ethclient.rs
+++ coins/ethereum/src/ethclient.rs
@@ -121,6 +121,16 @@ impl EthClient {
         Ok(balance.to_string())
     }
 
+    async fn allowance(
+        &self,
+        address: ethers::types::Address,
+    ) -> Result<String, Error> {
+        let client = Arc::new(self.ethers());
+        let contract_instance = ERC20::new(address, Arc::clone(&client));
+        let balance = &contract_instance.balance_of(address).call().await.unwrap();
+        Ok(balance.to_string())
+    }
+
     /// Given a specified contract instance, determine the total supply of
     /// tokens
     async fn total_supply(&self, address: ethers::types::Address) -> Result<U256, Error> {
