ac8a33a8996703b5dddc035cd3c98c4c2b7b01bb
Karl Buys <kbbuys@gmail.com>
2023-08-04 12:12:59 +0200

    docs: adds example contract deployal from solidity

--- coins/ethereum/examples/deploy_contract.rs
+++ coins/ethereum/examples/deploy_contract.rs
@@ -1,15 +1,36 @@
+use ethers::{
+    contract::{abigen, ContractFactory},
+    core::utils::Anvil,
+    middleware::SignerMiddleware,
+    providers::{Http, Provider},
+    signers::{LocalWallet, Signer},
+    solc::{Artifact, Project, ProjectPathsConfig},
+};
+// use eyre::Result;
+use std::{path::PathBuf, sync::Arc, time::Duration};
+
 use ethers::prelude::*;
-use ethers_solc;
 use walletd_ethereum::prelude::*;
 use walletd_ethereum::Error;
 use walletd_hd_key::prelude::*;
 
-use std::{convert::TryFrom, path::Path, sync::Arc, time::Duration};
+use std::{convert::TryFrom, path::Path};
+
+abigen!(
+    SimpleContract,
+    r#"[
+        function setValue(string)
+        function getValue() external view returns (string)
+        event ValueChanged(address indexed author, string oldValue, string newValue)
+    ]"#,
+    event_derives(serde::Deserialize, serde::Serialize)
+);
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    // 1. compile the contract (note this requires that you are inside the `examples` directory) and
+    // 1. compile the contract
     // launch anvil
+    let anvil = Anvil::new().spawn();
     
     // set the path to the contract, `CARGO_MANIFEST_DIR` points to the directory containing the
     // manifest of `example/contracts`. which will be `../` relative to this file
@@ -23,7 +44,7 @@ async fn main() -> Result<(), Error> {
 
     // 3. connect to the network
     let provider =
-        Provider::<Http>::try_from(anvil.endpoint())?.interval(Duration::from_millis(10u64));
+        Provider::<Http>::try_from(anvil.endpoint()).unwrap().interval(Duration::from_millis(10u64));
 
     // 4. instantiate the client with the wallet
     let client = SignerMiddleware::new(provider, wallet.with_chain_id(anvil.chain_id()));
@@ -33,23 +54,24 @@ async fn main() -> Result<(), Error> {
     let factory = ContractFactory::new(abi, bytecode, client.clone());
 
     // 6. deploy it with the constructor arguments
-    let contract = factory.deploy("initial value".to_string())?.send().await?;
+    let contract = factory.deploy("initial value".to_string()).unwrap().send().await.unwrap();
 
     // 7. get the contract's address
     let addr = contract.address();
-
+    println!("Contract address: {}", addr);
+    
     // 8. instantiate the contract
     let contract = SimpleContract::new(addr, client.clone());
 
     // 9. call the `setValue` method
     // (first `await` returns a PendingTransaction, second one waits for it to be mined)
-    let _receipt = contract.set_value("hi".to_owned()).send().await?.await?;
+    let _receipt = contract.set_value("WalletD Ethereum contract deployal".to_owned()).send().await.unwrap().await.unwrap();
 
     // 10. get all events
-    let logs = contract.value_changed_filter().from_block(0u64).query().await?;
+    let logs = contract.value_changed_filter().from_block(0u64).query().await.unwrap();
 
     // 11. get the new value
-    let value = contract.get_value().call().await?;
+    let value = contract.get_value().call().await.unwrap();
 
     println!("Value: {value}. Logs: {}", serde_json::to_string(&logs)?);
 
