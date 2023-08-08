28a32fb0324c0fe13b95b512234a4453dabc35c2
Karl Buys <kbbuys@gmail.com>
2023-08-08 14:41:11 +0200

    feat: add gas oracle middleware

--- coins/ethereum/src/ethereum_wallet.rs
+++ coins/ethereum/src/ethereum_wallet.rs
@@ -6,7 +6,9 @@ use crate::Error;
 use crate::EthClient;
 use crate::{EthereumAmount, EthereumFormat};
 
+use ethers::middleware::gas_oracle::{GasCategory, GasNow, GasOracle};
 use ethers::prelude::*;
+use ethers::prelude::gas_oracle::GasOracleMiddleware;
 // use ethers::providers::{Middleware};
 // use ethers::types::{TransactionRequest};
 // use ethers::signers::{Signer};
@@ -313,6 +315,7 @@ impl EthereumWallet {
 
         // Link our wallet instance to our provider for signing our transactions
         let client = SignerMiddleware::new(provider, wallet_from_bytes.with_chain_id(5u64));
+        let client = GasOracleMiddleware::new(client, GasNow::new());
         // Create a transaction request to send 10000 wei to the Goerli address
         // TODO: Use gas oracle for more complex transactions where required gas is not known
         // 21000 = basic transfer
