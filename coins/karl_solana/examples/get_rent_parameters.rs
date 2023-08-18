af0eb40d9d15fe1c460a0353ca40aa80a86e6122
Karl Buys <kbbuys@gmail.com>
2023-08-18 10:25:58 +0200

    feat: borrow basic code from Ethereum example
    This will be reworked to align with Solana's way of working

---
+++ coins/karl_solana/examples/get_rent_parameters.rs
@@ -0,0 +1,23 @@
+use karl_solana::solanaclient;
+
+// // https://goerli.etherscan.io/block/8455626
+// // const PROVIDER_URL: &str = "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161";
+// // use ethers::prelude::*;
+// // use walletd_coin_core::BlockchainConnector;
+// // use walletd_ethereum::EthClient;
+// use karl_solana::SolanaClient;
+// // Works with ethers
+// #[tokio::main]
+// async fn main() {
+//     let solana_client = SolanaClient::new(PROVIDER_URL).unwrap();
+//     println!("{:#?}", solana_client);
+//     // let _block_number: U64 = U64::from(8455626);
+//     // print!("block_number: {:?}", &_block_number);
+//     // let _latest_block_data = EthClient::latest_block(&eth_client).await.is_err();
+
+//     // assert!(!_latest_block_data);
+//     // print!("If you see this, it means that block 8455626 was retrieved without error.");
+// }
+fn main() {
+    
+}
