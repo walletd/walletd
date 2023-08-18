36205517576e6823cabfae5b96c3bafc01b15e75
Karl Buys <kbbuys@gmail.com>
2023-08-18 19:57:17 +0200

    feat: get minimum fee for specified data length

--- coins/karl_solana/examples/get_rent_parameters.rs
+++ coins/karl_solana/examples/get_rent_parameters.rs
@@ -26,4 +26,8 @@ async fn main() {
     println!("Ok, invoked");
     let latest_blockhash = solana_client.rpc_client().get_latest_blockhash().await.unwrap();
     println!("{:?}", latest_blockhash);
+
+    let data_len = 300;
+    let balance = solana_client.rpc_client().get_minimum_balance_for_rent_exemption(data_len).await.unwrap();
+    println!("Minimum fee to be rent-exempt: {}", balance);
 }
