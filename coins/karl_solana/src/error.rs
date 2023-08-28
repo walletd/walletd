2a0ac2d7053ebcda8148b20b7190657ea35178ba
Karl Buys <kbbuys@gmail.com>
2023-08-28 13:50:43 +0200

    refactor: removed non-solana errors

--- coins/karl_solana/src/error.rs
+++ coins/karl_solana/src/error.rs
@@ -2,7 +2,7 @@ use thiserror::Error;
 
 /// Custom error type for this crate.
 #[derive(Error, Debug)]
-pub enum SolanaError {
+pub enum Error {
     // /// Error due to missing public key
     // #[error("Public Key not included")]
     // MissingPublicKey,
@@ -18,33 +18,12 @@ pub enum SolanaError {
     // /// Unable to import wallet, missing info to import a specific wallet
     // #[error("Unable to import wallet: {0}")]
     // UnableToImportWallet(String),
-    // /// Error downcasting to EthereumWallet
-    // #[error("Could not downcast to EthereumWallet")]
-    // UnableToDowncastWallet,
-    // /// Error downcasting a blockchain connector
-    // #[error("Error downcasting: {0}")]
-    // UnableToDowncastBlockchainConnector(String),
     // /// Error converting to a type when parsing from a string
     // #[error("Error converting to a type from a string: {0}")]
     // FromStr(String),
-    // /// Error from secp256k1
-    // #[error("Error from secp256k1: {0}")]
-    // SerdeJson(#[from] secp256k1::Error),
     // /// Converted ParseInt error
     // #[error("ParseInt error: {0}")]
     // ParseInt(#[from] std::num::ParseIntError),
-    // // Convert web3 error
-    // // #[error("web3 error: {0}")]
-    // // FromWeb3(#[from] web3::Error),
-    // /// Error related to converting from or to a hex
-    // #[error("Hex error: {0}")]
-    // Hex(#[from] hex::FromHexError),
-    // /// Error from the walletd_hd_key crate
-    // #[error("Error from walletd_hd_key: {0}")]
-    // WalletdHDKey(#[from] walletd_hd_key::Error),
-    // /// Error when trying to initialize EthClient
-    // #[error("Failed to initialize EthClient")]
-    // EthClientInit,
     // /// Error related to a transaaction
     // #[error("Failed to retrieve data for transaction: {0}")]
     // TxResponse(String),
@@ -60,10 +39,4 @@ pub enum SolanaError {
     // /// Error due to overflow
     // #[error("Overflow error: {0}")]
     // Overflow(String),
-    // Error from web3 contract
-    // #[error("Error from web3 contract: {0}")]
-    // Web3Contract(#[from] web3::contract::Error),
-    // Error from web3 ethabi
-    // #[error("Error from web3 ethabi: {0}")]
-    // Web3Ethabi(#[from] web3::ethabi::Error),
 }
