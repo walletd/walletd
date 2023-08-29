311ef8d4cec7dbbb355f8694a181a62514e3ed5f
Karl Buys <kbbuys@gmail.com>
2023-08-29 15:23:25 +0200

    refactor: rename module name to correct name

--- coins/solana/examples/get_rent_parameters.rs
+++ coins/solana/examples/get_rent_parameters.rs
@@ -1,23 +1,4 @@
-use karl_solana::solanaclient::SolanaClient;
-
-// // https://goerli.etherscan.io/block/8455626
-// // const PROVIDER_URL: &str = "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161";
-// // use ethers::prelude::*;
-// // use walletd_coin_core::BlockchainConnector;
-// // use walletd_ethereum::EthClient;
-// use karl_solana::SolanaClient;
-// // Works with ethers
-// #[tokio::main]
-// async fn main() {
-//     let solana_client = SolanaClient::new(PROVIDER_URL).unwrap();
-//     println!("{:#?}", solana_client);
-//     // let _block_number: U64 = U64::from(8455626);
-//     // print!("block_number: {:?}", &_block_number);
-//     // let _latest_block_data = EthClient::latest_block(&eth_client).await.is_err();
-
-//     // assert!(!_latest_block_data);
-//     // print!("If you see this, it means that block 8455626 was retrieved without error.");
-// }
+use walletd_solana::solanaclient::SolanaClient;
 
 const URL: &str = "http://127.0.0.1:8899";
 #[tokio::main]
