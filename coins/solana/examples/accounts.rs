75afcb23b3b58e73e2e0f6553d7e2369c78c7f7a
Karl Buys <kbbuys@gmail.com>
2023-08-30 15:35:18 +0200

    docs: provide example of balance retrieval for a pubkey

--- coins/solana/examples/accounts.rs
+++ coins/solana/examples/accounts.rs
@@ -12,20 +12,23 @@ async fn main() {
 
     let solana_client = SolanaClient::new(&rpc_url).unwrap();
     
-    let new_keypair = Keypair::new();
-    println!("new keypair: {:?}", &new_keypair);
-    let from_pubkey = Signer::pubkey(&from_keypair);
-    println!("public key: {:?}", from_pubkey);
+    // let new_keypair = Keypair::new();
+    // println!("new keypair: {:?}", &new_keypair);
+    // let from_pubkey = Signer::pubkey(&new_keypair);
+    // println!("public key: {:?}", from_pubkey);
 
     // Restore an account from a 64 byte array
     let bytes: [u8; 64] = [162, 101, 169, 19, 38, 115, 20, 31, 216, 254, 39, 215, 229, 185, 248, 68, 251, 0, 232, 164, 241, 72, 249, 89, 84, 169, 54, 223, 127, 161, 21, 23, 69, 199, 131, 221, 202, 170, 155, 110, 8, 211, 170, 217, 132, 148, 104, 122, 117, 238, 217, 1, 90, 103, 0, 46, 176, 210, 139, 14, 213, 254, 7, 120]; 
     let restored_keypair = Keypair::from_bytes(&bytes).unwrap();
 
-    let from_bytes = from_keypair.to_bytes();
-    println!("From bytes: {:?}", from_bytes);
+    println!("From bytes: {:?}", &bytes);
     println!("Restored: {:?}", restored_keypair);
 
-    let from_pubkey = Signer::pubkey(from_keypair);
+    let from_pubkey = Signer::pubkey(&restored_keypair);
+    println!("public key: {:?}", from_pubkey);
+
+    let balance = solana_client.get_balance(&from_pubkey).await.unwrap();
+    println!("Balance: {}", balance);
 
     // match connection.request_airdrop(&from_pubkey, LAMPORTS_PER_SOL) {
     //     Ok(sig) => loop {
