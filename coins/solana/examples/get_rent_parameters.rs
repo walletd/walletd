271ed8fab13b7bbee7a376d2b85a80123e81888f
Karl Buys <kbbuys@gmail.com>
2023-08-30 10:40:28 +0200

    docs: give example of how to retrieve minimum rent

--- coins/solana/examples/get_rent_parameters.rs
+++ coins/solana/examples/get_rent_parameters.rs
@@ -4,11 +4,11 @@ const URL: &str = "http://127.0.0.1:8899";
 #[tokio::main]
 async fn main() {
     let solana_client = SolanaClient::new(URL).unwrap();
-    println!("Ok, invoked");
     let latest_blockhash = solana_client.rpc_client().get_latest_blockhash().await.unwrap();
-    println!("{:?}", latest_blockhash);
+    println!("Latest blockhaish: {:?}", latest_blockhash);
 
     let data_len = 300;
     let balance = solana_client.rpc_client().get_minimum_balance_for_rent_exemption(data_len).await.unwrap();
-    println!("Minimum fee to be rent-exempt: {}", balance);
+    println!("Assuming a 300 byte contract, the minimum fee to be rent-exempt is: {}", balance);
+    
 }
