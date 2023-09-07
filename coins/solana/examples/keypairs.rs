a1c2ef47c8cbe0e40ac2465fdef3deb1ee283dc7
Karl Buys <kbbuys@gmail.com>
2023-09-07 09:02:19 +0200

    feat: deterministically generate additional keypairs

--- coins/solana/examples/keypairs.rs
+++ coins/solana/examples/keypairs.rs
@@ -1,12 +1,20 @@
 use solana_sdk::signature::{Keypair};
 use solana_sdk::pubkey::{Pubkey};
+use solana_sdk::signature::{Signer};
 use std::str::FromStr;
 
 fn main() {
-
+    // Use this example script to generate your own unique keypair (pubkey plus signing key)
     // Create new keypair
     // Note that Keypair::new() will always give a public key that is valid for users
+    // Run this from the terminal by using the command `cargo run --example accounts
     let wallet = Keypair::new();
+
+    let base58_wallet_string = &wallet.to_base58_string();
+    let public_key = Signer::pubkey(&wallet);
+
+    println!("base58: {:?}" , &base58_wallet_string);
+    println!("pubkey: {:?}" , &public_key);
 
     // Restore a keypair from bytes
     let secret_key: [u8; 64] = [
@@ -20,17 +28,17 @@ fn main() {
 
     // Restore a wallet from a base58 string
     let wallet_from_base58 = Keypair::from_base58_string(
-        "5MaiiCavjCmn9Hs1o3eznqDEhRwxo7pXiAYez7keQUviUkauRiTMD8DrESdrNjN8zd9mTmVhRvBJeg5vhyvgrAhG",
+        &base58_wallet_string
     );
 
     // Checking if a public key has an associated ed25519 private key 
     // In certain special cases (e.g. a Program Derived Address), public keys may not have a private key associated with them. You can check this by looking to see if the public key lies on the ed25519 curve. Only public keys that lie on the curve can be controlled by users with wallets.
 
-    let pubkey = Pubkey::from_str("5oNDL3swdJJF1g9DzJiZ4ynHXgszjAEpUkxVYejchzrY").unwrap(); // Valid public key
-    println!("{:?}", pubkey.is_on_curve()); // Lies on the ed25519 curve and is suitable for users
+    let pubkey = Signer::pubkey(&wallet_from_base58); // Valid public key
+    println!("Pubkey is on ed curve: {:?}", pubkey.is_on_curve()); // Lies on the ed25519 curve and is suitable for users
 
     let off_curve_address = Pubkey::from_str("4BJXYkfvg37zEmBbsacZjeQDpTNx91KppxFJxRqrz48e").unwrap(); // Valid public key
-    println!("{:?}", off_curve_address.is_on_curve()); // Not on the ed25519 curve, therefore not suitable for users
+    println!("Pubkey is off curve (for system programs): {:?}", off_curve_address.is_on_curve()); // Not on the ed25519 curve, therefore not suitable for users
 
     let error_pubkey = Pubkey::from_str("testPubkey").unwrap(); // Is not a valid public key
 }
