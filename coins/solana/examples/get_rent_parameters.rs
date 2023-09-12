db61ad839125a30516c0439021a98fc9679cc374
Karl Buys <kbbuys@gmail.com>
2023-09-12 17:21:45 +0200

    fix: fix rent parameter URL

--- coins/solana/examples/get_rent_parameters.rs
+++ coins/solana/examples/get_rent_parameters.rs
@@ -1,14 +1,14 @@
 use walletd_solana::solanaclient::SolanaClient;
 
-const URL: &str = "http://127.0.0.1:8899";
 #[tokio::main]
 async fn main() {
-    let solana_client = SolanaClient::new(URL).unwrap();
+    let rpc_url = "https://api.devnet.solana.com";
+    let solana_client = SolanaClient::new(rpc_url).await.unwrap();
     let latest_blockhash = solana_client.rpc_client().get_latest_blockhash().await.unwrap();
-    println!("Latest blockhaish: {:?}", latest_blockhash);
+    println!("Latest blockhash: {:?}", latest_blockhash);
 
     let data_len = 300;
     let balance = solana_client.rpc_client().get_minimum_balance_for_rent_exemption(data_len).await.unwrap();
     println!("Assuming a 300 byte contract, the minimum fee to be rent-exempt is: {}", balance);
-    
+    ()
 }
