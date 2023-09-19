ab1486e6b6b95592327b4a2593d568eebbceed1f
Karl Buys <kbbuys@gmail.com>
2023-09-19 14:36:09 +0200

    refactor: massively cleans up accounts.rs example code

--- coins/solana/examples/accounts.rs
+++ coins/solana/examples/accounts.rs
@@ -4,57 +4,33 @@ use solana_sdk::system_instruction;
 use solana_sdk::commitment_config::CommitmentConfig;
 use solana_sdk::native_token::LAMPORTS_PER_SOL;
 use solana_sdk::signature::{Keypair, Signer};
+use solana_sdk::pubkey::Pubkey;
+use std::convert::TryFrom;
 
 #[tokio::main]
 async fn main() {
 
     let rpc_url = String::from("https://api.devnet.solana.com");
-    // default
+    // Old style
     let connection = RpcClient::new_with_commitment(&rpc_url, CommitmentConfig::confirmed());
-    // propriety
+    // WalletD style
     let solana_client = SolanaClient::new(&rpc_url).await.unwrap();
-    
+    let connected_client = solana_client.rpc_client();
     // We need a pubkey
     // Wallet from base58
 
-    let from_wallet_string = "g6mLsmgPznVcEcSLDWQ9QGuhNFa96CaC6R2XCnivHNfJ2aujuC3Cy9dSVvG39XMsGkuXEn1yYfauErro9LX5FyX".to_string();
-    let from_wallet = Keypair::from_base58_string(&from_wallet_string);
-    let pubkey = from_wallet.pubkey();
-    println!("pubkey: {:?}", &pubkey);
-    
-    let acc_info = connection.get_account(&pubkey).unwrap();
+    let pubstr = "zRgZGarWLpmZsDQPPCvzaxxsCxk6xcTNc97sKYxbXQy";
+    let pubkey_result: Pubkey = pubstr.parse().unwrap();
+    println!("public key: {:?}", &pubkey_result);
+    let acc_info = connected_client.get_account(&pubkey_result).await.unwrap();
     println!("Account data {:?}", acc_info);
-
-    println!("key: {:?}", &pubkey);
+    
+    println!("key: {:?}", &pubkey_result);
     println!("lamports: {:?}", acc_info.lamports);
     println!("data: {:?}", acc_info.data); 
     println!("owner: {:?}", acc_info.owner);
     println!("rent_epoch: {:?}", acc_info.rent_epoch);
-    // println!("is_signer: {:?}", acc_info.is_signer);
-    // println!("is_writable: {:?}", acc_info.is_writable);
     println!("executable: {:?}", acc_info.executable);
-
-
-    // let acc_info_res = acc_info.unwrap();
-    // match acc_info_res {
-    //     Ok(acc: Account) => acc_info {
-    //         println!("Account data {:?}", acc);
-    //     },
-    //     Err(e) => {
-    //         println!("Error: {:?}", e);
-    //         return;
-    //     }
-    // };
-
-    
-
-
-    // OLD IGNORE BELOW
-    
-    // let new_keypair = Keypair::new();
-    // println!("new keypair: {:?}", &new_keypair);
-    // let from_pubkey = Signer::pubkey(&new_keypair);
-    // println!("public key: {:?}", from_pubkey);
 
     // Restore an account from a 64 byte array
     let bytes: [u8; 64] = [162, 101, 169, 19, 38, 115, 20, 31, 216, 254, 39, 215, 229, 185, 248, 68, 251, 0, 232, 164, 241, 72, 249, 89, 84, 169, 54, 223, 127, 161, 21, 23, 69, 199, 131, 221, 202, 170, 155, 110, 8, 211, 170, 217, 132, 148, 104, 122, 117, 238, 217, 1, 90, 103, 0, 46, 176, 210, 139, 14, 213, 254, 7, 120]; 
@@ -64,57 +40,19 @@ async fn main() {
     println!("Restored: {:?}", restored_keypair);
 
     let from_pubkey = Signer::pubkey(&restored_keypair);
-    println!("public key: {:?}", from_pubkey);
+    println!("public key: {:?}", &from_pubkey);
 
-    let balance = solana_client.get_balance(&from_pubkey).await.unwrap();
+    let balance = connected_client.get_balance(&from_pubkey).await.unwrap();
     println!("Balance: {}", balance);
 
-    // match connection.request_airdrop(&from_pubkey, LAMPORTS_PER_SOL) {
-    //     Ok(sig) => loop {
-    //         if let Ok(confirmed) = connection.confirm_transaction(&sig) {
-    //             if confirmed {
-    //                 println!("Transaction: {} Status: {}", sig, confirmed);
-    //                 break;
-    //             }
-    //         }
-    //     },
-    //     Err(_) => println!("Error requesting airdrop"),
-    // };
+    let acc_info = connected_client.get_account(&from_pubkey).await.unwrap();
+    println!("Account data {:?}", acc_info);
 
-    // let space = 0;
-    // let rent_exemption_amount = connection
-    //     .get_minimum_balance_for_rent_exemption(space)
-    //     .unwrap();
+    println!("key: {:?}", &pubkey_result);
 
-    // let new_account_keypair = Keypair::new();
-    // let new_account_pubkey = Signer::pubkey(&new_account_keypair);
-
-    // let create_account_ix = system_instruction::create_account(
-    //     &from_pubkey,
-    //     &new_account_pubkey,
-    //     rent_exemption_amount,
-    //     space as u64,
-    //     &from_pubkey,
-    // );
-
-    // let (recent_blockhash, _) = connection.get_recent_blockhash().unwrap();
-
-    // let create_account_tx = solana_sdk::transaction::Transaction::new_signed_with_payer(
-    //     &[create_account_ix],
-    //     Some(&from_pubkey),
-    //     &[&from_keypair, &new_account_keypair],
-    //     recent_blockhash,
-    // );
-
-    // match connection.send_and_confirm_transaction(&create_account_tx) {
-    //     Ok(sig) => loop {
-    //         if let Ok(confirmed) = connection.confirm_transaction(&sig) {
-    //             if confirmed {
-    //                 println!("Transaction: {} Status: {}", sig, confirmed);
-    //                 break;
-    //             }
-    //         }
-    //     },
-    //     Err(_) => println!("Error creating system account"),
-    // };
+    println!("lamports: {:?}", acc_info.lamports);
+    println!("data: {:?}", acc_info.data); 
+    println!("owner: {:?}", acc_info.owner);
+    println!("rent_epoch: {:?}", acc_info.rent_epoch);
+    println!("executable: {:?}", acc_info.executable);
 }
