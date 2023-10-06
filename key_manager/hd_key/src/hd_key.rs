c0090b82cc687093af503bce44b7c05353834afc
Karl Buys <kbbuys@gmail.com>
2023-10-06 11:37:27 +0200

    refactor: remove zeroize tests

--- key_manager/hd_key/src/hd_key.rs
+++ key_manager/hd_key/src/hd_key.rs
@@ -677,42 +677,5 @@ mod tests {
         .unwrap();
         assert_eq!(keys.extended_public_key_serialized().unwrap(), "xpub661MyMwAqRbcFXMyiJX7c6ibHGtcUga5EJ5AGk2wpmtJToYC21K3osXhNPGsUzwLzHJDKShvbH6ZAHF4DB3eCKK9ya271pXyWABaBjRPorF")
     }
-
-    // #[test]
-    // fn test_hdkey_zeroize() -> Result<(), Error> {
-    //     let mut keys = HDKey::new_master(
-    //         Seed::new(vec![
-    //             162, 253, 156, 5, 34, 216, 77, 82, 238, 76, 133, 51, 220, 2, 212, 182, 155, 77,
-    //             249, 182, 37, 94, 26, 242, 12, 159, 29, 77, 105, 22, 137, 242, 163, 134, 55, 235,
-    //             30, 199, 120, 151, 43, 248, 69, 195, 45, 90, 232, 60, 117, 54, 153, 155, 86, 102,
-    //             57, 122, 195, 32, 33, 178, 30, 10, 204, 238,
-    //         ]),
-    //         HDNetworkType::MainNet,
-    //     )
-    //     .unwrap();
-    //     assert!(&keys.extended_private_key.is_some());
-    //     keys.zeroize();
-    //     assert_eq!(&keys.master_seed.as_bytes(), &[]);
-    //     assert!(&keys.extended_private_key.is_none());
-    //     Ok(())
-    // }
-
-    // #[test]
-    // fn test_extended_private_key_zeroize() -> Result<(), Error> {
-    //     let keys = HDKey::new_master(
-    //         Seed::new(vec![
-    //             162, 253, 156, 5, 34, 216, 77, 82, 238, 76, 133, 51, 220, 2, 212, 182, 155, 77,
-    //             249, 182, 37, 94, 26, 242, 12, 159, 29, 77, 105, 22, 137, 242, 163, 134, 55, 235,
-    //             30, 199, 120, 151, 43, 248, 69, 195, 45, 90, 232, 60, 117, 54, 153, 155, 86, 102,
-    //             57, 122, 195, 32, 33, 178, 30, 10, 204, 238,
-    //         ]),
-    //         HDNetworkType::MainNet,
-    //     )
-    //     .unwrap();
-    //     assert!(&keys.extended_private_key.is_some());
-    //     let mut extended_private_key = keys.extended_private_key()?;
-    //     extended_private_key.zeroize();
-    //     assert_eq!(extended_private_key.to_bytes(), [1u8; 32]);
-    //     Ok(())
-    // }
+    
 }
